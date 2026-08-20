d15d5bf007c71932d5cf0c8b402705592f6b0cfc
68160065-del <68160065@go.buu.ac.th>
2026-08-20 07:41:08 +0700

    Friend

---
+++ XOGUI/src/main/java/com/mycompany/friend/Friend.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.friend;
+
+/**
+ *
+ * @author admin
+ */
+public class Friend {
+    private String name;
+    private int age;
+    private String tel;
+
+    public Friend(String name, int age, String tel) {
+        this.name = name;
+        this.age = age;
+        this.tel = tel;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Friend{" + "name=" + name + ", age=" + age + ", tel=" + tel + '}';
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+    
+}
