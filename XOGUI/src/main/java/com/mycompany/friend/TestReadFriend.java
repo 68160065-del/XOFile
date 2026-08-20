27a2cf88dc573d01bdce3f69ad4b9bf76324ed95
68160065-del <68160065@go.buu.ac.th>
2026-08-20 09:23:23 +0700

    Test Read Friend

---
+++ XOGUI/src/main/java/com/mycompany/friend/TestReadFriend.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.friend;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+/**
+ *
+ * @author admin
+ */
+public class TestReadFriend {
+    public static void main(String[] args){
+        File file = new File("friend.dat");
+        try {
+            FileInputStream fis = new FileInputStream(file);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            Friend f1 = (Friend) ois.readObject();
+            Friend f2 = (Friend) ois.readObject();
+            System.out.println(f1);
+            System.out.println(f2);
+            ois.close();
+            fis.close();
+        } catch (FileNotFoundException ex) {
+            System.getLogger(TestReadFriend.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
+        } catch (IOException ex) {
+            System.getLogger(TestReadFriend.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
+        } catch (ClassNotFoundException ex) {
+            System.getLogger(TestReadFriend.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
+        }
+        
+    }
+}
